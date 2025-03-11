c6a44664ca032fc9a0ce0868435d0a927202e814
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-03-11 17:14:13 -0600

    Added code to test out pox and see how it works

--- Max_Donaldson_u1409474.py
+++ Max_Donaldson_u1409474.py
@@ -1,1 +1,4 @@
-
+def launch (**kw):
+    print("this is a test, if this prints out; then it works!")
+    message = "this is another test, if it displays this -- pox is confusing"
+    return message
