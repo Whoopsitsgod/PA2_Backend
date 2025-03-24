7074455e85d8437b86b0a068064efb7802a17bb1
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-03-24 11:41:41 -0600

    added basic test funcitonality

--- Max_Donaldson_u1409474.py
+++ Max_Donaldson_u1409474.py
@@ -1,4 +1,16 @@
+# my (Max Donaldson's) function
+import pox.proto.arp_responder as arp_responder
+from pox.core import core
+import pox
+
 def launch (**kw):
     print("this is a test, if this prints out; then it works!")
-    message = "this is another test, if it displays this -- pox is confusing"
-    return message
+    newObby = ICantThinkOfAGoodName()
+
+class ICantThinkOfAGoodName ():
+    def __init__ (self):
+        print("test")
+        core.addListener(self)
+
+    def _handle_PacketIn (self, event):
+        print("packet!")
