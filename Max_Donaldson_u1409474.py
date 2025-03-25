16da0fd930ab5defc5dc9510934b2733b707939a
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-03-24 18:26:36 -0600

    Update Max_Donaldson_u1409474.py

--- Max_Donaldson_u1409474.py
+++ Max_Donaldson_u1409474.py
@@ -8,23 +8,14 @@ from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
 def launch (**kw):
     print("this is a test, if this prints out; then it works!")
     newObby = ICantThinkOfAGoodName()
-    core.registerNew(ARPResponder)
 
 class ICantThinkOfAGoodName ():
     def __init__ (self):
         print("test")
-        core.addListeners(self)
+        core.openflow.addListeners(self)
 
     def _handle_PacketIn (self, event):
         print("packet!")
 
     def _handle_GoingUpEvent (self, event):
-        core.openflow.addListeners(self)
         print("another test, pray to lordy it works")
-
-    def _handle_ConnectionUp (self, event):
-      fm = of.ofp_flow_mod()
-      fm.priority -= 0x1000 # lower than the default
-      fm.match.dl_type = ethernet.ARP_TYPE
-      fm.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
-      event.connection.send(fm)
