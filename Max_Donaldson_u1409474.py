87fc974f0d76a847158f6acb65ca5989de3ddbe5
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-03-26 16:33:22 -0600

    Update Max_Donaldson_u1409474.py

--- Max_Donaldson_u1409474.py
+++ Max_Donaldson_u1409474.py
@@ -27,6 +27,10 @@ class MyComponent (object):
   def __init__ (self):
     core.openflow.addListeners(self)
     self.roundRobinSendToH5 = True
+    #initialize hardcode table
+    self.hardcodeDictionary = {}
+    self.hardcodeDictionary[IPAddr()]
+    #initialize 
  
   def _handle_ConnectionUp (self, event):
     print(f"Switch %s has come up.", dpid_to_str(event.dpid))
@@ -36,11 +40,9 @@ class MyComponent (object):
     fm.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
     event.connection.send(fm)
 
-  # SHOULD fire when s1 (the controller/switch) recieves a packet, but does not all the time.
   def _handle_PacketIn (self, event):
-    print(f"packet recieved!")
-    log.debug(event.port)
-
+    log.debug("packet recieved!")
+    #packet handled!
     dpid = event.connection.dpid
     inport = event.port
     packet = event.parsed
@@ -55,54 +57,63 @@ class MyComponent (object):
       log.debug("Packet did not contain ARP request")
       return
     
-    mac = event.connection.eth_addr
-    sourceIp = a.hwsrc
-
     #DICTIONARY
     # DPID = source MAC address
     # mac = also source MAC address
     # protosrc = source IP address
     # protodest = desired IP address
+    mac = event.connection.eth_addr
     log.debug("this is the source MAC address " + dpid_to_str(dpid))
     log.debug("this is the protosrc (?) " + str(a.protosrc))
     log.debug("this is the mac address (?) " + str(mac))
     log.debug("this is the protodest (?) " + str(a.protodst))
     
-    # ask if this is a reasonable way of doing things, if h1 is now always
-    # going to
-    if self.roundRobinSendToH5:
-      r = arp()
-      r.hwtype = a.hwtype
-      r.prototype = a.prototype
-      r.hwlen = a.hwlen
-      r.protolen = a.protolen
-      r.opcode = arp.REPLY
-      r.hwdst = a.hwsrc
-      r.protodst = a.protosrc
-      r.protosrc = a.protodst
-      log.debug("alright, we're getting to the danger zone")
-      r.hwsrc = EthAddr("00:00:00:00:00:05")
-      log.debug("past the danger zone!")
-      e = ethernet(type=packet.type, src=event.connection.eth_addr,
-      dst=a.hwsrc)
-      e.payload = r
-      if packet.type == ethernet.VLAN_TYPE:
-        log.debug("whoopsies, this was needed")
-        v_rcv = packet.find('vlan')
-        e.payload = vlan(eth_type = e.type,
+    macToSend = None
+    # ask if this is a reasonable way of doing things!
+    # this handles figuring out where the MAC address to send to is
+    if mac is EthAddr("00:00:00:00:00:05") or EthAddr("00:00:00:00:00:06"):
+      # if the message came from h5 or h6, then use the hardcoded table to find where the 
+      macToSend = self.hardcodeDictionary[a.protodst]
+    else:
+      if self.roundRobinSendToH5:
+        macToSend = EthAddr("00:00:00:00:00:05")
+        self.roundRobinSendToH5 = False
+      else:
+        macToSend = EthAddr("00:00:00:00:00:06")
+        self.roundRobinSendToH5 = True
+      self.hardcodeDictionary[a.protodst] = mac
+
+    #send the arp_response
+    r = arp()
+    r.hwtype = a.hwtype
+    r.prototype = a.prototype
+    r.hwlen = a.hwlen
+    r.protolen = a.protolen
+    r.opcode = arp.REPLY
+    r.hwdst = a.hwsrc
+    r.protodst = a.protosrc
+    r.protosrc = a.protodst
+    log.debug("alright, we're getting to the danger zone")
+    r.hwsrc = macToSend
+    log.debug("past the danger zone!")
+    e = ethernet(type=packet.type, src=event.connection.eth_addr,
+    dst=a.hwsrc)
+    e.payload = r
+    if packet.type == ethernet.VLAN_TYPE:
+      log.debug("whoopsies, this was needed")
+      v_rcv = packet.find('vlan')
+      e.payload = vlan(eth_type = e.type,
                                  payload = e.payload,
                                  id = v_rcv.id,
                                  pcp = v_rcv.pcp)
-        e.type = ethernet.VLAN_TYPE
-      log.debug("%s answering ARP for %s" % (dpid_to_str(dpid),
+      e.type = ethernet.VLAN_TYPE
+    log.debug("%s answering ARP for %s" % (dpid_to_str(dpid),
                 str(r.protosrc)))
-      msg = of.ofp_packet_out()
-      msg.data = e.pack()
-      msg.actions.append(of.ofp_action_output(port = of.OFPP_IN_PORT))
-      msg.in_port = inport
-      event.connection.send(msg)
-    else:
-      log.debug("this shouldn't ever fire!")
+    msg = of.ofp_packet_out()
+    msg.data = e.pack()
+    msg.actions.append(of.ofp_action_output(port = of.OFPP_IN_PORT))
+    msg.in_port = inport
+    event.connection.send(msg)
     
   def _handle_GoingUpEvent (self, event):
     #this doesn't fire, for... some reason.
