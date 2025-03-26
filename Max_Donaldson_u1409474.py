4fc0fb3193a1c32e2a6057f92aa1d1783cf068fb
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-03-25 23:47:27 -0600

    Update Max_Donaldson_u1409474.py

--- Max_Donaldson_u1409474.py
+++ Max_Donaldson_u1409474.py
@@ -1,4 +1,5 @@
 # my (Max Donaldson's) function
+
 from pox.core import core
 import pox
 log = core.getLogger()
@@ -20,27 +21,37 @@ def launch ():
 
 class MyComponent (object):
   def __init__ (self):
+    core.addListeners(self)
+
+  def _handle_GoingUpEvent (self, event):
     core.openflow.addListeners(self)
+    log.debug("Up...")
  
   def _handle_ConnectionUp (self, event):
     print(f"Switch %s has come up.", dpid_to_str(event.dpid))
-         
+    fm = of.ofp_flow_mod()
+    fm.priority -= 0x1000 # lower than the default
+    fm.match.dl_type = ethernet.ARP_TYPE
+    fm.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
+    event.connection.send(fm)
+
+  # SHOULD fire when s1 (the controller/switch) recieves a packet, but does not all the time.
   def _handle_PacketIn (self, event):
     print(f"packet recieved!")
     log.debug(event.port)
 
     dpid = event.connection.dpid
     packet = event.parsed
-    a = packet.find('arp')
+    # check if packet parsed correctly
+    if not packet.parsed:
+      log.warning("%s: ignoring unparsed packet", dpid_to_str(dpid))
+      return
     
+    a = packet.find('arp')
+    # if packet didn't contain an ARP request
     if not a: 
       log.debug("Packet did not contain ARP request")
       return
-
+    
     mac = event.connection.eth_addr
     sourceIp = a.hwsrc
-
-    
-    
-  def _handle_GoingUpEvent (self, event):
-    log.debug("Up...")
