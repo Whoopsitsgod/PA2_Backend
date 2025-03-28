678140ecf1220fbe6d8b6a54b55e3c51636b47f1
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-03-28 02:31:44 -0600

    Update Max_Donaldson_u1409474.py

--- Max_Donaldson_u1409474.py
+++ Max_Donaldson_u1409474.py
@@ -30,7 +30,12 @@ class MyComponent (object):
     self.roundRobinSendToH5 = True
     #initialize hardcode table
     self.hardcodeDictionary = {}
+    self.hardcodeDictionary["10.0.0.5"] = EthAddr("00:00:00:00:00:05")
+    self.hardcodeDictionary["10.0.0.6"] = EthAddr("00:00:00:00:00:06")
     self.connectionTable = {}
+    self.portTable = {}
+    self.portTable["10.0.0.5"] = 5
+    self.portTable["10.0.0.6"] = 6
     #initialize 
  
   def _handle_ConnectionUp (self, event):
@@ -63,33 +68,45 @@ class MyComponent (object):
       # protosrc = source IP address
       # protodest = desired IP address
       mac = event.connection.eth_addr
-      log.debug("this is the source MAC address " + dpid_to_str(dpid))
-      log.debug("this is the protosrc (?) " + str(a.protosrc))
-      log.debug("this is the mac address (?) " + str(mac))
-      log.debug("this is the protodest (?) " + str(a.protodst))
-      
+      log.debug("============= CONNECTION SOURCE INFO =============")
+      log.debug("this is the source IP (protosrc): " + str(a.protosrc))
+      log.debug("this is the MAC adress (hwsrc): " + str(a.hwsrc))
+      log.debug("this is the destination IP (protodst): " + str(a.protodst))
+      log.debug("this is the port: " + str(inport))
+      log.debug("============= CONNECTION SOURCE INFO =============")
+
       macToSend = None
       # TODO: ask if this is a reasonable way of doing things!
       # this handles figuring out where the MAC address to send to is
-      if str(mac) == "00:00:00:00:00:05" or str(mac) == "00:00:00:00:00:06":
+      if str(a.hwsrc) == "00:00:00:00:00:05" or str(a.hwsrc) == "00:00:00:00:00:06":
         # if the message came from h5 or h6, then use the hardcoded table to find where the 
-        macToSend = self.hardcodeDictionary[str(a.protodst)]
+        log.debug("a.protodst is...." + str(a.protodst))
+        connectedHost2 = self.connectionTable[str(a.protosrc)]
+        macToSend = self.hardcodeDictionary[connectedHost2]
+        log.debug("the mac to send to is... " + str(macToSend))
       else:
         # TODO: Add a method that prevents if the arp is sent quickly before it can respond. It can sometimes be assigned to *BOTH* h5 and h6
         # now do round robin!
         if str(a.protosrc) in self.connectionTable:
-          macToSend = self.connectionTable[str(a.protosrc)]
+          connectedHost = self.connectionTable[str(a.protosrc)]
+          log.debug("connectedhost is...." + connectedHost)
+          macToSend = self.hardcodeDictionary[connectedHost]
         else:
           if self.roundRobinSendToH5:
             macToSend = EthAddr("00:00:00:00:00:05")
-            self.connectionTable[str(a.protosrc)] = macToSend
+            self.connectionTable["10.0.0.5"] = str(a.protosrc)
+            self.connectionTable[str(a.protosrc)] = "10.0.0.5"
             self.roundRobinSendToH5 = False
           else:
             macToSend = EthAddr("00:00:00:00:00:06")
-            self.connectionTable[str(a.protosrc)] = macToSend
+            self.connectionTable["10.0.0.6"] = str(a.protosrc)
+            self.connectionTable[str(a.protosrc)] = "10.0.0.6"
             self.roundRobinSendToH5 = True
           # add the source's IP address to the table to it's related MAC address
-          self.hardcodeDictionary[str(a.protosrc)] = mac
+          self.hardcodeDictionary[str(a.protosrc)] = a.hwsrc
+
+      #add to the port table
+      self.portTable[str(a.protosrc)] = inport
 
       #send the arp_response
       r = arp()
@@ -101,22 +118,17 @@ class MyComponent (object):
       r.hwdst = a.hwsrc
       r.protodst = a.protosrc
       r.protosrc = a.protodst
-      log.debug("alright, we're getting to the danger zone")
       r.hwsrc = macToSend
-      log.debug("past the danger zone!")
       e = ethernet(type=packet.type, src=event.connection.eth_addr,
       dst=a.hwsrc)
       e.payload = r
       if packet.type == ethernet.VLAN_TYPE:
-        log.debug("whoopsies, this was needed")
         v_rcv = packet.find('vlan')
         e.payload = vlan(eth_type = e.type,
                                   payload = e.payload,
                                   id = v_rcv.id,
                                   pcp = v_rcv.pcp)
         e.type = ethernet.VLAN_TYPE
-      log.debug("%s answering ARP for %s" % (dpid_to_str(dpid),
-                  str(r.protosrc)))
       msg = of.ofp_packet_out()
       msg.data = e.pack()
       msg.actions.append(of.ofp_action_output(port = of.OFPP_IN_PORT))
@@ -129,18 +141,25 @@ class MyComponent (object):
       flowRules.match.inport = inport
       flowRules.match.dl_type = pkt.ethernet.IP_TYPE
       flowRules.match.nw_dst = IPAddr("10.0.0.10")
-
-      flowRules.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr("10.0.0.10")))
+      
+      actualIpDest = self.connectionTable[str(a.protosrc)]
+      actualPortDest = self.portTable[actualIpDest]
+      flowRules.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr(actualIpDest)))
       flowRules.actions.append(of.ofp_action_dl_addr.set_dst(macToSend))
-      # TODO: CHANGE! THIS IS STATIC!
-      flowRules.actions.append(of.ofp_action_output(port=5))
+      flowRules.actions.append(of.ofp_action_nw_addr.set_src(IPAddr("10.0.0.10")))
+      flowRules.actions.append(of.ofp_action_output(port=actualPortDest))
+      log.debug("============= LINKING INFO =============")
+      log.debug("Will be sent to IP: " + actualIpDest)
+      log.debug("Will be sent to MAC ADDRESS: " + str(macToSend))
+      log.debug("Will be sent to port: " + str(actualPortDest))
+      log.debug("============= LINKING INFO =============")
+      event.connection.send(flowRules)
 
     #this is the race condition prevention 
     elif packet.type == pkt.ethernet.IP_TYPE:
-      print("placeholder")
+      log.debug("recieved IP_TYPE packet")
       packetMsg = of.ofp_packet_out()
-      packetMsg = of.ofp_packet_out(port = of.OFPP_TABLE)
       packetMsg.data = event.ofp
-      event.connection.send(msg)
+      event.connection.send(packetMsg)
 
   # tcpdump -n -i h1-eth0
